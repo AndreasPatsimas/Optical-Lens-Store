67f432f30ad3230a6145fa5b26a1bc94e1829c4d
Andreas Patsimas <45208441+AndreasPatsimas@users.noreply.github.com>
2021-11-02 12:58:39 +0200

    first commit

---
+++ src/main/java/org/patsimas/optical_lens_store/OpticalLensStoreApplication.java
@@ -0,0 +1,13 @@
+package org.patsimas.optical_lens_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpticalLensStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpticalLensStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/patsimas/optical_lens_store/OpticalLensStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.patsimas.optical_lens_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpticalLensStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
