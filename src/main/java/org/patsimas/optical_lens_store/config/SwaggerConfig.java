dc8c99352bc42aff5aa77ffcfd20bad1f4ff407b
Andreas Patsimas <45208441+AndreasPatsimas@users.noreply.github.com>
2021-11-22 09:47:47 +0200

    swagger

---
+++ src/main/java/org/patsimas/optical_lens_store/config/SwaggerConfig.java
@@ -0,0 +1,53 @@
+package org.patsimas.optical_lens_store.config;
+
+import com.google.common.base.Predicate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.SecurityConfiguration;
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.google.common.base.Predicates.or;
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket mainGroup() {
+        return new Docket(DocumentationType.SWAGGER_2).groupName("a-group: main")
+                .apiInfo(apiInfo())
+                .select()
+                .paths(mainPath())
+                .build();
+    }
+
+    private Predicate<String> mainPath() {
+
+        return or(
+                regex("/customers.*"),
+                regex("/orders.*")
+        );
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("Optical Lens Store API")
+                .description("Optical Lens Store API reference for developers")
+                .termsOfServiceUrl("http://javainuse.com")
+                .contact("apatsimas@solcrowe.gr").license("Optical Lens Store License")
+                .licenseUrl("apatsimas@solcrowe.gr").version("1.0").build();
+    }
+}
